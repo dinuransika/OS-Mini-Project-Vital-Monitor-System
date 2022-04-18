5ab7d102f626df73b7387dba0d15932dc612fc60
Dinura Dissanayake <dinuraransikadissanayake@gmail.com>
2022-04-18 23:28:47 +0530

    Changed the structure

--- Gateway.java
+++ Gateway.java
@@ -6,17 +6,23 @@
 //////////////////////////////////////////////////////////////////////////////
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 
-// Implement a gateway that discovers all of these vital monitors.
-// Gateway class extends Thread class. 
-public class Gateway extends Thread{
+// Implement a gateway that discovers all of these vital monitors. 
+public class Gateway{
     // Creating the data gram socket for packet recieving on port 6000
-    private DatagramSocket createRecieveSocket(){
+    private static DatagramSocket createRecieveSocket(){
         DatagramSocket socket = null;
         try {
             socket = new DatagramSocket(6000);
@@ -26,41 +32,94 @@ public class Gateway extends Thread{
         return socket;
     }
     // Recieve datagram packets from the vital monitor
-    private DatagramPacket recievePacket(){
+    private static DatagramPacket recievePacket(DatagramSocket socket){
         DatagramPacket packet = null;
         try {
             packet = new DatagramPacket(new byte[1024], 1024);
+            socket.receive(packet);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return packet;
     }
-    // Use the run mehtod to recieve the vital monitor packets concurrently
-    private void Run(Gateway gateway){
-        DatagramSocket recieveSocket = gateway.createRecieveSocket();
-        DatagramPacket recievePacket = gateway.recievePacket();
-        // Recieve the vital monitor packets
-        while(true){
-            try {
-                recieveSocket.receive(recievePacket);
-                ByteArrayInputStream bis = new ByteArrayInputStream(recievePacket.getData()); // Get the monitor object from the packet as a Byte array input stream
-                ObjectInputStream ois = new ObjectInputStream(bis); // Convert the Byte array input stream to an object input stream
-                Monitor monitor = (Monitor) ois.readObject(); // Convert the object input stream to a monitor object
-                // Create a TCP connection and recieve the message from the vital monitor.
-                Socket client = new Socket(monitor.getIp(), monitor.getPort());
-                byte[] buffer = new byte[1024];
-                client.getInputStream().read(buffer);
-                String message = new String(buffer);
-                System.out.println("Message: " + message); // Printing the message from the vital monitor
-                client.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+
+
+    private static void createSocket(Monitor monitor){
+        try {
+            Socket client = new Socket(monitor.getIp(), monitor.getPort());
+            // System.out.println("Establishing a connection to Monitor ID: " + monitor.getMonitorID() + " IP: " + monitor.getIp() + " PORT:" + monitor.getPort());
+            OutputStream os = client.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            osw.write("Request: " + monitor.getMonitorID());
+            osw.flush();
+            recieveMessages(client);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }        
+    }
+
+    private static Monitor getMonitor(DatagramSocket recieveSocket, DatagramPacket recievePacket){
+        Monitor monitor = null;
+        try{
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(recievePacket.getData());
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+            monitor = (Monitor) objectInputStream.readObject();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return monitor;
+    }
+
+    private static void recieveMessages(Socket client){
+        try {
+            InputStream is = client.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            StringBuffer sb = new StringBuffer();
+            while(true){
+                int i = isr.read();
+                while(i != '\n'){
+                    sb.append((char)i);
+                    i = isr.read();
+                }
+                if (sb.toString().equals("end")){
+                    break;
+                }
+                System.out.println(sb.toString());
+                sb.delete(0, sb.length());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        Gateway gateway = new Gateway();
-        gateway.Run(gateway);
+        DatagramSocket recieveSocket = createRecieveSocket();
+        // Recieve the vital monitor packets
+
+        // list to store ips of the monitors
+        List<String> monitors = new ArrayList<String>();
+
+        while(true){
+            DatagramPacket recievePacket = recievePacket(recieveSocket);
+            Monitor monitor = getMonitor(recieveSocket, recievePacket);
+            InetAddress ipAdd = monitor.getIp();
+            int port = monitor.getPort();
+            if (!monitors.contains(ipAdd+":"+port)) {
+                monitors.add(ipAdd + ":" + port);
+    
+                System.out.println("Establishing a connection to Monitor ID: " + recievePacket.getAddress().toString() + " IP: " + recievePacket.getAddress().getHostAddress() + " PORT:" + recievePacket.getPort());
+                
+                // Create a new thread to send the vital monitor packets
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    synchronized public void run() {
+                        createSocket(monitor);
+                    }
+                });
+                thread.start();
+                
+            }
+        }
     }
 }
